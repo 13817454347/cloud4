8a145f603dcbc5f6380804846540996542ec7ef9
13817454347 <13817454347@163.com>
2020-10-13 09:49:35 +0800

    test

--- product-data-service/src/main/java/cn/how2j/springcloud/service/ProductService.java
+++ product-data-service/src/main/java/cn/how2j/springcloud/service/ProductService.java
@@ -12,10 +12,10 @@ import cn.how2j.springcloud.pojo.Product;
 public class ProductService {
     @Value("${server.port}")
     String port;
-    
+
 	public List<Product> listProducts(){
     	List<Product> ps = new ArrayList<>();
-    	ps.add(new Product(1,"product a from port:"+port, 50));
+    	ps.add(new Product(1,"product a from port:"+port, 500));
     	ps.add(new Product(2,"product b from port:"+port, 150));
     	ps.add(new Product(3,"product c from port:"+port, 250));
     	return ps;
