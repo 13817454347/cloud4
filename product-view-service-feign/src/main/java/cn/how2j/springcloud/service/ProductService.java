924bb6498dcaf83b2e6f40676e8f861cfd8a2bcb
13817454347 <13817454347@163.com>
2020-10-13 09:37:44 +0800

    test

---
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/service/ProductService.java
@@ -0,0 +1,19 @@
+package cn.how2j.springcloud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+import cn.how2j.springcloud.client.ProductClientFeign;
+import cn.how2j.springcloud.pojo.Product;
+
+@Service
+public class ProductService {
+	@Autowired ProductClientFeign productClientFeign;
+	public List<Product> listProducts(){
+		return productClientFeign.listProdcuts();
+
+	}
+}
